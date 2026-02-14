8d6963141ef606198ff88b6413fd09ac324554dd
MinaElHanash <139964160+MinaElHanash@users.noreply.github.com>
2026-02-14 21:30:49 +0200

    Phase_1

---
+++ Program_Counter.v
@@ -0,0 +1,19 @@
+module Program_Counter (
+    input [31:0] Next_PC,
+    input rst, clk,
+    output reg [31:0] Current_PC
+);
+    
+    always @(posedge clk or posedge rst) // asynchronous reset, PC will reset regardless of th clk
+        begin
+            if (rst) 
+                begin
+                    Current_PC <= 32'd0;
+                end
+            else 
+                begin
+                    Current_PC <= Next_PC;
+                end
+        end
+
+endmodule
